784242da299907eb01733962feb3515e8446ec40
naojitaniguchi <naoji.taniguchi@gmail.com>
2020-10-08 22:10:49 +0900

    First commit.

---
+++ Assets/ActivateByTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateByTrigger : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A key pushed");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.gameObject.tag == "Player")
+        {
+            target.SetActive(true);
+        }
+    }
+}
